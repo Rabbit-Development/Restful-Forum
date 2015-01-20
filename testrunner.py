b181e97079da83096dbd80be5bc44122322e8396
unknown <alfmags@googlemail.com>
2015-01-20 21:49:33 +0100

    Alfs første test

--- testrunner.py
+++ testrunner.py
@@ -3,10 +3,12 @@ from restforum import controller
 from restforum.models import *
 from flask.ext.mongoengine import MongoEngine
 from mongoengine import connect
-import unittest, json
+import unittest, json, datetime
 
 class controllerTestCase(unittest.TestCase):
 
+
+	
 	def setUp(self):
 		controller.config['MONGODB_SETTINGS'] = {
 			'db':'test_db',
@@ -38,6 +40,28 @@ class controllerTestCase(unittest.TestCase):
 		rv = self.controller.post('/register', data=json.dumps(user_info), headers={'content-type':'application/json'})
 		assert "User registered" in rv.data.decode('utf-8')
 		assert 200 == rv.status_code
+
+	def test_post(self):
+
+		post_info = {
+
+			'body' :'Kveldens første test',
+			'title' : 'Kveldens første title',
+			'author' : "Alf Magnus skrev dette"
+
+		}
+
+		d = json.dumps(post_info)
+
+		
+
+		rv = self.controller.post('/post', data=d, headers={'content-type':'application/json'})
+
+		assert "Post finished" in rv.data.decode('utf-8')
+		assert 200 == rv.status_code
+
+
+
 
 if __name__ == '__main__':
 	unittest.main()
